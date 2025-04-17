3ca43e01f5f5638655f8e31ed1859f00cc10e9f8
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-04-17 20:31:51 +0530

    config file setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,29 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    chromeWebSecurity: false,
+    scrollBehavior: false,  // to prevent cypress' default behaviour to scroll elements into focus
+
+    /* numTestsKeptInMemory: 50 (default), --> making it 0 disables timetravel
+       this also helps to log element yeild on console after clicking element */
+    numTestsKeptInMemory: 50,
+
+    experimentalMemoryManagement: true,
+    requestTimeout: 90000,
+    responseTimeout: 90000,
+    defaultCommandTimeout: 40000,
+    pageLoadTimeout: 120000,  // used for cy.visit()
+    testIsolation: false,    // no logout after each it block and data is preserved across test suite
+    video: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    watchForFileChanges: false,
+    slowTestThreshold: 180_000,
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
